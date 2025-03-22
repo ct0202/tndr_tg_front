98f1560443f48c9273ef0f51aa3166f84b679ddc
ct0202 <arman.jumashev.dev@gmail.com>
2025-03-22 14:00:33 +0500

    svg objects on premium

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -12,7 +12,8 @@ function Premium() {
             <div className="w-full h-[661px] mt-[100px] relative rounded-[16px] border-[1px] border-[lightgrey]">
                 <img src="/images/icons/gradient.svg" className="w-full"/>
                 <img src="/images/icons/logo_premium.svg" className="w-[220px] ml-4 absolute top-[80px]"/>
-                <img src="/images/icons/close_button_premium_page.svg" className="absolute top-2 right-2" />
+                {/*<img src="/images/icons/close_button_premium_page.svg" className="absolute top-2 right-2"/>*/}
+                <object data="/images/icons/close_button_premium_page.svg" type="image/svg+xml" className="absolute top-2 right-2"/>
 
                 <div className="mt-[40px] ml-4 flex flex-col gap-[7px]">
                     <div className="w-full flex flex-row gap-[10px]">
@@ -50,15 +51,20 @@ function Premium() {
 
                 <div className="w-full text-sm text-center mt-7 flex items-center justify-center flex-col">
                     <span>Как получить 14 дней <span className="text-[#ED3144]">Премиума</span> БЕСПЛАТНО?</span>
-                    <img src="/images/icons/premium_invite_button.svg"/>
+                    {/*<img src="/images/icons/premium_invite_button.svg"/>*/}
+                    <object data="/images/icons/premium_invite_button.svg" type="image/svg+xml"/>
                 </div>
 
                 <div className="w-full flex flex-col text-center items-center justify-center">
                     <span>Или поддержите проект подпиской</span>
                     <div className="w-full flex flex-row items-center justify-center">
-                        <img src="/images/icons/buy_week.svg"/>
-                        <img src="/images/icons/buy_month.svg"/>
-                        <img src="/images/icons/buy_3_month.svg"/>
+                        {/*<img src="/images/icons/buy_week.svg"/>*/}
+                        {/*<img src="/images/icons/buy_month.svg"/>*/}
+                        {/*<img src="/images/icons/buy_3_month.svg"/>*/}
+
+                        <object data="/images/icons/buy_week.svg" type="image/svg+xml"/>
+                        <object data="/images/icons/buy_month.svg" type="image/svg+xml"/>
+                        <object data="/images/icons/buy_3_month.svg" type="image/svg+xml"/>
                     </div>
                 </div>
             </div>
