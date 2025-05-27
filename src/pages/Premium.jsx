e5da0bd82a8471b8e7a87d0d4e9f479e4cb9f432
B0user <56859914+B0user@users.noreply.github.com>
2025-05-27 19:18:08 +0500

    Update Premium.jsx

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -5,9 +5,10 @@ import Button from "../components/Button";
 function Premium() {
 
     const [link, setLink] = useState("");
-    const [duration, setDuration] = useState("week")
+    const [duration, setDuration] = useState("week");
+    // const [type, setType] = useState("2_weeks");
 
-    async function handleBuy() {
+    async function handleBuy(type) {
         if (!window.Telegram?.WebApp) return;
 
         try {
@@ -33,7 +34,7 @@ function Premium() {
 
             // console.log(server_response);
 
-            const result = await axios.post('/createInvoiceLink', {type: "1_month"});
+            const result = await axios.post('/createInvoiceLink', {type: type});
             console.log("RESULT FROM AXIOS",result);
             window.Telegram.WebApp.openInvoice(result.data.result, (status) => {
                 alert("Invoice closed with status:", status);
@@ -60,26 +61,19 @@ function Premium() {
         }
     }
 
-
-    function handleTestButton() {
-        // This button will just trigger the handleBuy method to check the invoice functionality
-        handleBuy();
-    }
-
-
     return (
         <div
             className="w-[95vw] flex flex-col justify-start items-center"
             style={{ height: "calc(100% - 80px)" }}
         >
-            <div className="w-full text-center mt-4">
+            {/* <div className="w-full text-center mt-4">
                 <button 
                     onClick={handleTestButton}
                     className="px-6 py-3 bg-blue-500 text-white rounded-xl"
                 >
                     Test Invoice Method
                 </button>
-            </div>
+            </div> */}
             
             <div className="w-full h-[661px] mt-[100px] relative rounded-[16px] border-[1px] border-[lightgrey]">
                 <img src="/images/icons/gradient.svg" className="w-full"/>
@@ -121,7 +115,7 @@ function Premium() {
                 </div>
 
                 <div className="w-full text-sm text-center mt-7 flex items-center justify-center flex-col" >
-                    <span>Как получить 14 дней <span className="text-[#ED3144]" onClick={()=>handleBuy()}>Премиума</span> БЕСПЛАТНО?</span>
+                    <span>Как получить 14 дней <span className="text-[#ED3144]">Премиума</span> БЕСПЛАТНО?</span>
                     {/*<img src="/images/icons/premium_invite_button.svg"/>*/}
                     <object data="/images/icons/premium_invite_button.svg" type="image/svg+xml" />
                 </div>
@@ -134,21 +128,21 @@ function Premium() {
                         {/*<img src="/images/icons/buy_3_month.svg"/>*/}
                         <div className="relative flex">
                             <object data="/images/icons/primary%20button.svg" type="image/svg+xml" />
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]" onClick={()=>handleBuy()}>
+                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]" onClick={()=>handleBuy("1_week")}>
                                 <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">200₽</span>
                                 <span className="pt-[4px] text-[14px] text-[#ED3144]">Неделя</span>
                             </div>
                         </div>
                         <div className="relative flex">
                             <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]">
+                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("1_month")}>
                                 <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">500₽</span>
                                 <span className="pt-[4px] text-[14px] text-[#ED3144]">Месяц</span>
                             </div>
                         </div>
                         <div className="relative flex">
                             <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]">
+                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("3_months")}>
                                 <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">1200₽</span>
                                 <span className="pt-[4px] text-[14px] text-[#ED3144]">3 Месяца</span>
                             </div>
