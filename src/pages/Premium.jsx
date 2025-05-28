7f026f95a66f5489e91ed0899002fa11127102eb
B0user <56859914+B0user@users.noreply.github.com>
2025-05-28 14:07:26 +0500

    theories

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -34,14 +34,54 @@ function Premium() {
 
             // console.log(server_response);
 
-            const result = await axios.post('/createInvoiceLink', {type: type});
+            // const result = await axios.post('/createInvoiceLink', {type: type});
+            const result = await axios.post(
+                  `https://api.telegram.org/bot8193869137:AAFifGJF9t66MPcU5d_DFWvbfAwmufnOhlU/createInvoiceLink`,
+                  {
+                    title: "Подписка Премиум",
+                    description: "7 дней подписки Премиум",
+                    payload: "premium_7_days",
+                    provider_token: "390540012:LIVE:70096",
+                    currency: "RUB",
+                    prices: [
+                      { label: "Подписка на 2 недели", amount: 20000 }
+                    ],
+                    need_email: true,
+                    send_email_to_provider: true,
+                    need_phone_number: true,
+                    send_phone_number_to_provider: true,
+                    start_parameter: "premium7days",
+                    provider_data: JSON.stringify({
+                        receipt: {
+                            items: [
+                                {
+                                description: "Подписка на недел.",
+                                quantity: 1,
+                                amount: {
+                                    value: 200, // в рублях, не копейках!
+                                    currency: "RUB"
+                                },
+                                vat_code: 1,
+                                payment_mode: "full_payment",
+                                payment_subject: "service"
+                                }
+                            ],
+                            tax_system_code: 1
+                        }
+                    })
+                }
+            
+            );
+            
             console.log("RESULT FROM AXIOS",result);
+
+            // or maybe in the callback?
             window.Telegram.WebApp.openInvoice(result.data.result, async (result) => {
-                alert(result);
+                alert(result); // rn gives failed, then it cannot be connected to later lines, it is just in the createIvoiceLink method somewhere
             });
             
 
-
+            // or maybe in getting telegramId?
             const initData = window.Telegram.WebApp.initData;
             const params = new URLSearchParams(initData);
             const userData = params.get("user");
