f3e35cd223ff5eb0a8f068afe74f683fb43178cd
ct0202 <arman.jumashev.dev@gmail.com>
2025-06-01 19:07:29 +0300

    focus input

--- src/steps/Step1.jsx
+++ src/steps/Step1.jsx
@@ -38,7 +38,9 @@ function Step1() {
   console.log(filters)
   return (
       <div className='flex flex-col justify-start items-center'>
-        <p className= {`text-gray text-[20px] font-medium mt-[${inputTop}px]`} >Как тебя зовут?</p>
+        <p className= {`text-gray text-[20px] font-medium`}
+           style={{ marginTop: `${inputTop}px` }}
+        >Как тебя зовут?</p>
         <input
             type="text"
             className='w-[361px] h-[58px] rounded-[12px] mt-6 px-3 text-[32px]  focus:outline-none text-center'
