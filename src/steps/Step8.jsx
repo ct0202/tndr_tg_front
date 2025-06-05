8adce1319cb3a1204627d8c562201217de49e687
ct0202 <arman.jumashev.dev@gmail.com>
2025-06-05 16:13:37 +0500

    lst

--- src/steps/Step8.jsx
+++ src/steps/Step8.jsx
@@ -33,7 +33,7 @@ function Step8() {
             );
 
             console.log('Upload successful:', response.data);
-            filters.photos.push(response.data);
+            updateFilter("photos", [...(filters?.photos || []), response.data]);
         } catch (error) {
             console.error('Error uploading photo:', error);
         }
