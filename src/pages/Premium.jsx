4958090c4f30c380a41504d8929e2a39050b1c73
ct0202 <arman.jumashev.dev@gmail.com>
2025-06-13 12:47:39 +0500

    123

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -1,150 +1,54 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 function Premium() {
-
+    const navigate = useNavigate();
     const [link, setLink] = useState("");
     const [duration, setDuration] = useState("week");
+    const [isPremium, setIsPremium] = useState(false);
+    const [cancelling, setCancelling] = useState(false);
+    const [buySuccess, setBuySuccess] = useState(false);
+    const [invitedCount, setInvitedCount] = useState(0);
     // const [type, setType] = useState("2_weeks");
+
+    useEffect(() => {
+        axios.get('/ispremium/' + localStorage.getItem("userId"))
+        .then(response => {
+            console.log("isPremium response", response);
+            if (response.data?.isPremium) {
+                setIsPremium(true);
+            }
+            else {
+                setIsPremium(false);
+            }
+        })
+    }, []);
+
+
+    async function handleCancel() {
+        try {
+            const result = await axios.post('/cancelpremium/'+ localStorage.getItem("userId"));
+            console.log("cancel result", result);
+            if (result.status === 200) {
+                alert("Подписка успешно отменена.");
+                setCancelling(false);
+                setIsPremium(false);
+                navigate("/readyLogin");
+            } else {
+                alert("Не удалось отменить подписку. Попробуйте позже.");
+            }
+        } catch (error) {
+            console.error("Error cancelling subscription:", error.response || error.message);
+        }
+    }
 
     async function handleBuy(type) {
         if (!window.Telegram?.WebApp) return;
 
         try {
-            // let tgId = null;
-            // const params = new URLSearchParams(window.Telegram.WebApp.initData);
-            // const userData = params.get("user");
-            // if (userData) {
-            //     const userObj = JSON.parse(decodeURIComponent(userData));
-
-            //     if (!userObj.id) {
-            //     alert("Не удалось получить Telegram ID");
-            //     return;
-            //     }
-            //     tgId = userObj.id;
-
-            // };
-            // console.log(tgId);
-
-            // const server_response = await axios.post(`/users/givepremium`, {
-            //     telegramId: tgId,
-            //     duration: duration
-            // });
-
-            // console.log(server_response);
-
             const result = await axios.post('/createInvoiceLink', {type: type});
-            // const result = await axios.post(
-            //       `https://api.telegram.org/bot8193869137:AAHPVzF7MoMnpXK73bYOptLZSUSKqPjiSZk/createInvoiceLink`,
-            //       {
-            //         title: "Подписка Премиум",
-            //         description: "7 дней подписки Премиум",
-            //         payload: "premium_7_days",
-            //         provider_token: "390540012:LIVE:70096",
-            //         currency: "RUB",
-            //         prices: [
-            //           { label: "Подписка на 2 недели", amount: 20000 }
-            //         ],
-            //         need_email: true,
-            //         send_email_to_provider: true,
-            //         need_phone_number: true,
-            //         send_phone_number_to_provider: true,
-            //         start_parameter: "premium7days",
-            //         provider_data: JSON.stringify({
-            //             receipt: {
-            //                 items: [
-            //                     {
-            //                     description: "Подписка на недел.",
-            //                     quantity: 1,
-            //                     amount: {
-            //                         value: 200, // в рублях, не копейках!
-            //                         currency: "RUB"
-            //                     },
-            //                     vat_code: 1,
-            //                     payment_mode: "full_payment",
-            //                     payment_subject: "service"
-            //                     }
-            //                 ],
-            //                 tax_system_code: 1
-            //             }
-            //         })
-            //     }
-            
-            // );
-
-            // const planMap = {
-            //     "1_week": {
-            //     title: "Подписка Премиум",
-            //     description: "7 дней подписки Премиум",
-            //     priceRub: 200,
-            //     amount: 20000,
-            //     label: "Подписка на неделю",
-            //     start_parameter: "premium7days",
-            //     subscription_period: (7*24*3600)
-            //     },
-            //     "1_month": {
-            //     title: "Подписка Премиум",
-            //     description: "1 месяц подписки Премиум",
-            //     priceRub: 500,
-            //     amount: 50000,
-            //     label: "Подписка на 1 месяц",
-            //     start_parameter: "premium1month",
-            //     subscription_period: (30*24*3600)
-            //     },
-            //     "3_months": {
-            //     title: "Подписка Премиум",
-            //     description: "3 месяца подписки Премиум",
-            //     priceRub: 1200,
-            //     amount: 120000,
-            //     label: "Подписка на 3 месяца",
-            //     start_parameter: "premium3months",
-            //     subscription_period: (90*24*3600)
-            //     }
-            // };
-        
-            // const plan = planMap[type];
-        
-            // if (!plan) {
-            //     return;
-            // }
-        
-            // const result = await axios.post(
-            //     `https://api.telegram.org/bot8193869137:AAHPVzF7MoMnpXK73bYOptLZSUSKqPjiSZk/createInvoiceLink`,
-            //     {
-            //     title: plan.title,
-            //     description: plan.description,
-            //     payload: type,
-            //     provider_token: "390540012:LIVE:70096",
-            //     currency: "RUB",
-            //     prices: [{ label: plan.label, amount: plan.amount }],
-            //     need_email: true,
-            //     send_email_to_provider: true,
-            //     need_phone_number: true,
-            //     send_phone_number_to_provider: true,
-            //     subscription_period: plan.subscription_period,
-            //     recurring: true,
-            //     test:true,
-            //     start_parameter: plan.start_parameter,
-            //     provider_data: JSON.stringify({
-            //         receipt: {
-            //         items: [
-            //             {
-            //             description: plan.label,
-            //             quantity: 1,
-            //             amount: {
-            //                 value: plan.priceRub,
-            //                 currency: "RUB"
-            //             },
-            //             vat_code: 1,
-            //             payment_mode: "full_payment",
-            //             payment_subject: "service"
-            //             }
-            //         ],
-            //         tax_system_code: 1
-            //         }
-            //     })
-            // });
             console.log("RESULT FROM AXIOS",result);
 
             // or maybe in the callback?
@@ -163,13 +67,11 @@ function Premium() {
 
             window.Telegram.WebApp.onEvent('invoiceClosed', async function(object) {
             if (object.status == 'paid') {
-                // alert("PAYMENT SUCCESS NOW GIVE PREMIUM");
-
                 const new_result = await axios.post('/users/givepremium', {telegramId:tgId, duration:type});
                 console.log("premium resukt", new_result);
                 alert("Вы успешно оплатили подписку! Вам выдан ПРЕМИУМ");
-                
-                window.Telegram.WebApp.close();
+                setIsPremium(true);
+                setBuySuccess(true);
             } else if (object.status == 'failed') {
                 alert("Не беспокойтесь. Мы сохраним ваш выбор.");
             }
@@ -182,19 +84,16 @@ function Premium() {
 
     return (
         <div
-            className="w-[95vw] flex flex-col justify-start items-center"
+            className="w-[95vw] flex flex-col justify-  start items-center"
             style={{ height: "calc(100% - 80px)" }}
-        >
-            {/* <div className="w-full text-center mt-4">
-                <button 
-                    onClick={handleTestButton}
-                    className="px-6 py-3 bg-blue-500 text-white rounded-xl"
-                >
-                    Test Invoice Method
-                </button>
-            </div> */}
-            
-            <div className="w-full h-[661px] mt-[100px] relative rounded-[16px] border-[1px] border-[lightgrey]">
+        >   {buySuccess ? (
+                <div className="w-full text-center mt-4">
+                    <h2 className="text-lg font-semibold">Подписка оформлена!</h2>
+                    <p className="text-sm text-gray-500">Спасибо за покупку премиум подписки.</p>
+                </div>
+            ) : (
+                !cancelling ? (
+            <div className={`w-full ${isPremium ? "h-[535px]" : "h-[661px]"} mt-[100px] relative rounded-[16px] border-[1px] border-[lightgrey]`}>
                 <img src="/images/icons/gradient.svg" className="w-full"/>
                 <img src="/images/icons/logo_premium.svg" className="w-[220px] ml-4 absolute top-[80px]" />
                 <object data="/images/icons/close_button_premium_page.svg" type="image/svg+xml" className="absolute top-2 right-2"/>
@@ -233,44 +132,81 @@ function Premium() {
                     </div>
                 </div>
 
+
+                {!isPremium ? (
+                    <>
                 <div className="w-full text-sm text-center mt-7 flex items-center justify-center flex-col" >
                     <span>Как получить 14 дней <span className="text-[#ED3144]">Премиума</span> БЕСПЛАТНО?</span>
-                    {/*<img src="/images/icons/premium_invite_button.svg"/>*/}
-                    <object data="/images/icons/premium_invite_button.svg" type="image/svg+xml" />
+                    <Button onClick={() => {
+                            const link = "https://t.me/GoDateAppBot/GoDate?startapp=" + localStorage.getItem("userId"); 
+                            navigator.clipboard.writeText(link)
+                            .then(() => {
+                                alert("Ссылка скопирована!");
+                            })
+                            .catch(err => {
+                                console.error("Ошибка копирования:", err);
+                            });
+                        }} className='h-[50px] w-[316px] text-[16px] font-medium relative'>Пригласи 3 друзей {invitedCount}/3  <img src='/images/icons/copy_icon.png' className="absolute right-[13px] top-[14px]" width={22} height={22}/>  </Button>
                 </div>
 
                 <div className="w-full flex flex-col text-center items-center justify-center">
                     <span>Или поддержите проект подпиской</span>
                     <div className="w-full flex flex-row items-center justify-center">
-                        {/*<img src="/images/icons/buy_week.svg"/>*/}
-                        {/*<img src="/images/icons/buy_month.svg"/>*/}
-                        {/*<img src="/images/icons/buy_3_month.svg"/>*/}
-                        <div className="relative flex">
-                            <object data="/images/icons/primary%20button.svg" type="image/svg+xml" />
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]" onClick={()=>handleBuy("1_week")}>
-                                <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">200₽</span>
-                                <span className="pt-[4px] text-[14px] text-[#ED3144]">Неделя</span>
+
+
+
+                            <div className="relative flex">
+                                <object data="/images/icons/primary%20button.svg" type="image/svg+xml" />
+                                <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]" onClick={()=>handleBuy("1_week")}>
+                                    <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">200₽</span>
+                                    <span className="pt-[4px] text-[14px] text-[#ED3144]">Неделя</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="relative flex">
-                            <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("1_month")}>
-                                <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">500₽</span>
-                                <span className="pt-[4px] text-[14px] text-[#ED3144]">Месяц</span>
+                            <div className="relative flex">
+                                <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
+                                <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("1_month")}>
+                                    <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">500₽</span>
+                                    <span className="pt-[4px] text-[14px] text-[#ED3144]">Месяц</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="relative flex">
-                            <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
-                            <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("3_months")}>
-                                <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">1200₽</span>
-                                <span className="pt-[4px] text-[14px] text-[#ED3144]">3 Месяца</span>
+                            <div className="relative flex">
+                                <object data="/images/icons/primary%20button.svg" type="image/svg+xml"/>
+                                <div className="flex flex-col items-center justify-center absolute w-[105px] h-[64px]"  onClick={()=>handleBuy("3_months")}>
+                                    <span className="pt-[8px] text-[#F8A93C] font-normal text-[20px] leading-[0.8]">1200₽</span>
+                                    <span className="pt-[4px] text-[14px] text-[#ED3144]">3 Месяца</span>
+                                </div>
                             </div>
-                        </div>
-                        {/*<object data="/images/icons/buy_month.svg" type="image/svg+xml"/>*/}
-                        {/*<object data="/images/icons/buy_3_month.svg" type="image/svg+xml"/>*/}
+                   
+            
+
                     </div>
                 </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="mt-[32px] pb-[20px] pointer-events-auto" onClick={()=>setCancelling(true)}>
+                            {/* <object data="/images/icons/cancel_subscription.svg" type="image/svg+xml" className="w-full h-full" /> */}
+                            <img src = "/images/icons/cancel_subscription.png" className="w-full h-full" />
+                        </div>
+                    </>
+                )}
             </div>
+            ) : (
+                <div className="w-full mt-[300px] h-[187px] rounded-[16px] border-[1px] border-[lightgrey]"
+                style={{
+                    boxShadow: `
+                    0 0 2px 4px rgba(136, 146, 159, 0.10),
+                    0 0 4px 8px rgba(136, 146, 159, 0.10),
+                    `
+                }}>
+                    <p className="mt-[23px] mb-[32px] w-full text-center text-[20px] font-medium leading-[22px]">Вы уверены, что хотите<br/>отменить подписку?</p>
+                    <div className="w-full flex flex-row items-center justify-center gap-[10px] mt-[20px]">
+                        <img src="/images/icons/button_cancel_premium.png" width={154} height={64} onClick={handleCancel}/>
+                        <img src="/images/icons/button_keep_premium.png" width={154} height={64} onClick={() => navigate("/readyLogin")}/>
+                    </div>
+                </div>
+            ))}
+
         </div>
     );
 }
